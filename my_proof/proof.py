0c0376ffba7d75c3c976c11876c209cee0b7e2f1
Ryzer-X <devs@zlayer.ai>
2025-02-17 16:20:27 +0530

    feat: final build

--- my_proof/proof.py
+++ my_proof/proof.py
@@ -17,6 +17,21 @@ class Proof:
     def __init__(self, config: Dict[str, Any]):
         self.config = config
         self.proof_response = ProofResponse(dlp_id=config['dlp_id'])
+        self.wallet_address = ""
+    
+    def read_author_from_file(self, file_path: str):
+        """
+        Read parameters from a text file.
+
+        :param file_path: Path to the text file
+        :return: Tuple containing author, signature, and random_string
+        """
+        params = {}
+        with open(file_path, "r") as file:
+            for line in file:
+                key, value = line.strip().split(": ", 1)
+                params[key] = value
+        return params["author"]
 
     def generate(self) -> ProofResponse:
         """Generate proofs for all input files."""
@@ -27,12 +42,18 @@ class Proof:
             'valid': True,
         }
 
+        # Read the wallet address from the first .txt file in the input directory
+        txt_files = [f for f in os.listdir(self.config['input_dir']) if f.endswith('.txt')]
+        if txt_files:
+            self.wallet_address = self.read_author_from_file(os.path.join(self.config['input_dir'], txt_files[0])).lower()
+            logging.info(f"Wallet Address {self.wallet_address}")
+            
+            
         for input_filename in os.listdir(self.config['input_dir']):
-
             logging.info(f"Processing file: {input_filename}")
-            file_id = self.config.get('file_id') or 118
+            file_id = self.config.get('file_id') 
             logging.info(f"Processing file ID: {file_id}")
-            uniqueness_details = process_files_for_uniqueness(file_id, self.config['input_dir'], '0x1234')
+            uniqueness_details = process_files_for_uniqueness(file_id, self.config['input_dir'], self.wallet_address)
             quality_n_authenticity_details = process_files_for_quality_n_authenticity_scores(uniqueness_details.get("unique_csv_data"), uniqueness_details.get("unique_json_data"), uniqueness_details.get("unique_yaml_data"))
 
             # proof_response_object['ownership'] = 1.0
